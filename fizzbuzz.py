94bfbd925a50d095ed16d7283b0d4f2b9b51951c
Anthony <asecreti@maryhurst.org>
2024-07-29 23:04:55 -0400

    second commit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,11 @@
 # add your code here
 
+for num in range(1,101):
+    if num % 3 == 0:
+        print("Fizz")
+    if num % 5 == 0:
+        print("Buzz")
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    if num % 3 != 0 and num % 5 != 0:
+        print(num)
